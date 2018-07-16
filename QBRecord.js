07bd6fd402dea7cbc8b97685055ba5df30730914
Tristian Flanagan <tflanagan@datacollaborative.com>
2018-07-16 08:17:49 -0400

    src: attempt to resolve quickbase instance detection

--- QBRecord.js
+++ QBRecord.js
@@ -3,7 +3,7 @@
 /* Versioning */
 const VERSION_MAJOR = 1;
 const VERSION_MINOR = 4;
-const VERSION_PATCH = 14;
+const VERSION_PATCH = 15;
 
 /* Dependencies */
 const merge = require('lodash.merge');
@@ -49,7 +49,7 @@ class QBRecord {
 		this._fields = [];
 		this._meta = {};
 
-		if(options && options.quickbase.className && options.quickbase.className === 'QuickBase'){
+		if(options && options.quickbase && ((options.quickbase.className && options.quickbase.className === 'QuickBase') || typeof(options.quickbase.api) === 'function')){
 			this._qb = options.quickbase;
 
 			delete options.quickbase
