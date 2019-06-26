5f8dd52a0de1187e52fd9777fd84bbffed495e3e
Tristian Flanagan <tflanagan@datacollaborative.com>
2019-06-26 14:01:37 -0400

    deps: update quickbase

---
+++ build.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* Dependencies */
+const exec = require('child_process').exec;
+const join = require('path').join;
+const Promise = require('bluebird');
+
+/* Constants */
+const BABEL = join('.', 'node_modules', 'babel-cli', 'bin', 'babel.js');
+const BROWSERIFY = join('.', 'node_modules', 'browserify', 'bin', 'cmd.js');
+
+const SRC_FILE = join('.', 'QBRecord.js');
+const ES5_FILE = join('.', 'QBRecord.es5.js');
+const BROWSERIFY_FILE = join('.', 'QBRecord.browserify.js');
+const MINIFIED_FILE = join('.', 'QBRecord.browserify.min.js');
+
+/* Main */
+return new Promise((resolve, reject) => {
+	exec([
+		'node ' + BROWSERIFY + ' ' + SRC_FILE + ' > ' + BROWSERIFY_FILE,
+		'node ' + BABEL + ' --presets es2015 ' + BROWSERIFY_FILE + ' > ' + ES5_FILE,
+		'minify ' + ES5_FILE + ' > ' + MINIFIED_FILE,
+		'rm ' + ES5_FILE,
+		'rm ' + BROWSERIFY_FILE
+	].join(' && '), (err, stdout, stderr) => {
+		if (err)
+			return reject(new Error(err));
+
+		return resolve();
+	});
+});
