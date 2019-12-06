8c92557e7d0771629a99557fa050f1348352f8f5
Tristian Flanagan <tflanagan@datacollaborative.com>
2019-12-06 09:44:45 -0500

    deps: update field

--- QBRecord.js
+++ QBRecord.js
@@ -3,7 +3,7 @@
 /* Versioning */
 const VERSION_MAJOR = 2;
 const VERSION_MINOR = 0;
-const VERSION_PATCH = 13;
+const VERSION_PATCH = 14;
 
 /* Dependencies */
 const merge = require('lodash.merge');
