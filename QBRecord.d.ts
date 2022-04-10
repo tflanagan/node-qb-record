25ca4d16566214350a873771953c77a59c1d082a
Tristian Flanagan <tflanagan@datacollaborative.com>
2022-04-10 11:15:22 +0300

    types: update types

--- QBRecord.d.ts
+++ QBRecord.d.ts
@@ -10,7 +10,7 @@ export = QBRecord;
 declare class QBRecord<R = Record<string, any>> {
     constructor(options: Partial<QBRecordSettings<Record<keyof R, number>>>);
     _id: string;
-	_data: any;
+	_data: R;
 	_dbid: string;
 	_fids: Record<keyof R, number>;
 	_fields: QBField[];
@@ -19,24 +19,23 @@ declare class QBRecord<R = Record<string, any>> {
 	_rfc4122: any;
     className: string;
     settings: QBRecordSettings<Record<keyof R, number>>;
-	clear(): void;
-	delete(): Promise<QBRecord>;
+	clear(): this;
+	delete(): Promise<this>;
 	get<T extends keyof R>(field: T): R[T];
 	getDBID(): string;
 	getFid(field: number, byId: true): keyof R;
-	getFid(field: keyof R, byId: false): number;
-	getFid(field: keyof R | number, byId: boolean): keyof R | number;
+	getFid(field: keyof R, byId?: false): number;
 	getFids(): Record<keyof R, number>;
 	getField(id: number): QBField;
 	getFields(): QBField[];
 	getTableName(): string;
-	load(localQuery: string, localClist: string | (keyof R)[]): Promise<QBRecord>;
+	load(localQuery: string, localClist: string | (keyof R)[]): Promise<this>;
 	loadSchema(): Promise<QBField[]>;
-	save(fidsToSave?: (keyof R)[], reqHook?: ReqHook): Promise<QBRecord>;
-	set<T extends keyof R>(field: T, value?: R[T]): QBRecord;
-	setDBID(dbid: string): QBRecord;
-	setFid(name: keyof R, id: number): QBRecord;
-	setFids(fields: { name: keyof R; id: number; }[]): QBRecord;
+	save(fidsToSave?: (keyof R)[], reqHook?: ReqHook): Promise<this>;
+	set<T extends keyof R>(field: T, value?: R[T]): this;
+	setDBID(dbid: string): this;
+	setFid(name: keyof R, id: number): this;
+	setFids(fields: { name: keyof R; id: number; }[]): this;
 	toJson(fidsToConvert?: (keyof R)[]): R;
 }
 
